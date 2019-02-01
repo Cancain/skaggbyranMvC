4202eefb56c5a7d21d93c7d41fecc420b4e3865b
dlindgren86 <dennis.lindgren@gmail.com>
2019-02-01 12:15:16 +0100

    contactforms

--- app/views/users/editUser.php
+++ app/views/users/editUser.php
@@ -6,6 +6,7 @@
         <label class="graySm" for="userName">Användar-id: </label>
         <input class="grayForm formField" type="text" name="userId" readonly value="<?php echo $data['userId'] ?>">       
     </div>
+    <br>
     <div class="formBox">
         <label class="graySm" for="userName">Användarnamn: </label>
         <input class="formField" type="text" name="userName" value="<?php echo $data['userName'] ?>">
@@ -13,6 +14,7 @@
             <span class="warning"><?php echo $data['userNameErr'] ?></span>
         <?php endif;?>
     </div>
+    <br>
     <div class="formBox">
         <label class="graySm" for="firstName">Förnamn: </label>
         <input class="formField" type="text" name="firstName" value="<?php echo $data['firstName'] ?>">
@@ -20,6 +22,7 @@
             <span class="warning"><?php echo $data['firstNameErr'] ?></span>
         <?php endif;?>
     </div>
+    <br>
     <div class="formBox">
         <label class="graySm" for="lastName">Efternamn: </label>
         <input class="formField" type="text" name="lastName" value="<?php echo $data['lastName'] ?>">
@@ -27,6 +30,7 @@
             <span class="warning"><?php echo $data['lastNameErr'] ?></span>
         <?php endif;?>
     </div>
+    <br>
     <div class="formBox">
         <label class="graySm" for="email">Email: </label>
         <input class="formField" type="text" name="email" value="<?php echo $data['email'] ?>">
@@ -34,6 +38,7 @@
             <span class="warning"><?php echo $data['emailErr'] ?></span>
         <?php endif;?>
     </div>
+    <br>
     <div class="formBox">
         <label class="graySm" for="password">Ändra lösenord: </label>
         <input class="formField" type="password" name="password">
@@ -41,6 +46,7 @@
             <span class="warning"><?php echo $data['passwordErr'] ?></span>
         <?php endif;?>
     </div>
+    <br>
     <div class="formBox">
         <label class="graySm" for="confirmPassword">Bekräfta lösenord: </label>
         <input class="formField" type="password" name="confirmPassword">
@@ -48,14 +54,17 @@
             <span class="warning"><?php echo $data['confirmPasswordErr'] ?></span>
         <?php endif;?>
     </div>
+    <br>
     <div class="formBox">
         <label class="graySm">Kan editera</label>
         <input type="checkbox" name="isAdmin" value="true" <?php if($data['isAdmin']) echo 'checked' ?>>
     </div>
+    <br>
     <div class="formBox">
         <label class="graySm">Kan ändra användare</label>
         <input type="checkbox" name="superAdmin" value="true" <?php if($data['superAdmin']) echo 'checked' ?>>        
     </div>
+    
         <input class="formBtn" type="submit" value="Ändra">
         <input class="formBtn" type="button" value="Gå tillbaka"><br><br>
 </form>
