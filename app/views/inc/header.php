532d8f8ae41e78afcf18abd28385ef5951c44ed8
dlindgren86 <dennis.lindgren@gmail.com>
2019-02-08 10:19:09 +0100

    logosm

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <link rel="icon" href="<?php echo URLROOT; ?>img/1x/logoSm.png">
   <link rel="stylesheet" href="<?php echo URLROOT; ?>css/main.css" />
   <link rel="stylesheet" href="<?php echo URLROOT; ?>css/datepicker.min.css" />
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
